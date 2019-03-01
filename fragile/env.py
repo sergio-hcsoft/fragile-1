d559f271a7bdfe43515197ca64ba67dd065ea751
guillem <guillem.db@gmail.com>
2019-03-01 16:53:11 +0100

    Add environment interface for plangym

---
+++ fragile/env.py
@@ -0,0 +1,53 @@
+import numpy as np
+import torch
+from plangym.env import Environment
+from fragile.base_classes import BaseEnvironment
+from fragile.swarm import States
+from fragile.utils import to_numpy
+
+
+class DiscreteEnv(BaseEnvironment):
+    def __init__(self, env: Environment):
+        self._env = env
+        self._n_actions = self._env.action_space.n
+
+    @property
+    def n_actions(self):
+        return self._n_actions
+
+    def get_params_dict(self) -> dict:
+        params = {
+            "states": {"sizes": self._env.get_state().shape, "dtype": torch.int64},
+            "observs": {"sizes": self._env.observation_space.shape, "dtype": torch.float},
+            "rewards": {"sizes": tuple([1]), "dtype": torch.float},
+            "ends": {"sizes": tuple([1]), "dtype": torch.uint8},
+        }
+        return params
+
+    def step(self, actions, env_states, n_repeat_action: int = 1, *args, **kwargs) -> States:
+        states = to_numpy(env_states.states)
+        actions = to_numpy(actions).astype(np.int32)
+        new_states, observs, rewards, ends, infos = self._env.step_batch(
+            actions=actions, states=states, n_repeat_action=n_repeat_action
+        )
+
+        new_state = self._get_new_states(new_states, observs, rewards, ends, len(actions))
+        return new_state
+
+    def reset(self, batch_size: int = 1) -> States:
+        state, obs = self._env.reset()
+        states = np.array([state.copy() for _ in range(batch_size)])
+        observs = np.array([obs.copy() for _ in range(batch_size)])
+        rewards = np.zeros(batch_size, dtype=np.float32)
+        ends = np.zeros(batch_size, dtype=np.uint8)
+        new_states = self._get_new_states(states, observs, rewards, ends, batch_size)
+        return new_states
+
+    def _get_new_states(self, states, observs, rewards, ends, batch_size) -> States:
+        ends = np.array(ends, dtype=np.uint8).reshape(-1, 1)
+        rewards = np.array(rewards, dtype=np.float32).reshape(-1, 1)
+        observs = np.array(observs)
+        states = np.array(states)
+        state = States(state_dict=self.get_params_dict(), n_walkers=batch_size)
+        state.update(states=states, observs=observs, rewards=rewards, ends=ends)
+        return state
---
+++ fragile/tests/test_walkers.py
@@ -0,0 +1,109 @@
+import pytest
+import torch
+import numpy as np
+from fragile.states import States
+from fragile.swarm import Walkers
+from fragile.utils import relativize
+
+
+@pytest.fixture()
+def walkers():
+    n_walkers = 10
+    env_dict = {
+        "env_1": {"sizes": (1, 100)},
+        "env_2": {"sizes": (1, 33)},
+        "observs": {"sizes": (1, 100)},
+    }
+    model_dict = {"model_1": {"sizes": (1, 13)}, "model_2": {"sizes": (1, 5)}}
+
+    walkers = Walkers(
+        n_walkers=n_walkers, env_state_params=env_dict, model_state_params=model_dict
+    )
+    return walkers
+
+
+class TestWalkers:
+    def test_init(self, walkers):
+        pass
+
+    def test_repr_not_crashes(self, walkers):
+        assert isinstance(walkers.__repr__(), str)
+
+    def test_states_attributes(self, walkers):
+        assert isinstance(walkers.env_states, States)
+        assert isinstance(walkers.model_states, States)
+
+    def test_getattr(self, walkers):
+        assert isinstance(walkers.env_1, torch.Tensor)
+        assert isinstance(walkers.model_1, torch.Tensor)
+        assert isinstance(walkers.will_clone, torch.Tensor)
+        assert isinstance(walkers.obs, torch.Tensor)
+        with pytest.raises(AttributeError):
+            assert isinstance(walkers.moco, torch.Tensor)
+
+    def test_obs(self, walkers):
+        assert isinstance(walkers.obs, torch.Tensor)
+        walkers._env_states.observs = 10
+        with pytest.raises(TypeError):
+            walkers.obs
+
+        n_walkers = 10
+        env_dict = {"env_1": {"sizes": (1, 100)}, "env_2": {"sizes": (1, 33)}}
+        model_dict = {"model_1": {"sizes": (1, 13)}, "model_2": {"sizes": (1, 5)}}
+
+        walkers = Walkers(
+            n_walkers=n_walkers, env_state_params=env_dict, model_state_params=model_dict
+        )
+        with pytest.raises(AttributeError):
+            walkers.obs
+
+    def test_update_end_condition(self, walkers):
+        ends = np.zeros(10)
+        walkers.update_end_condition(ends)
+        assert torch.all(walkers.end_condition.cpu() == torch.zeros(10, dtype=torch.uint8))
+        ends = torch.ones(10, dtype=torch.uint8)
+        walkers.update_end_condition(ends)
+        assert torch.all(walkers.end_condition.cpu() == ends)
+
+    def test_calculate_end_condition(self, walkers):
+        walkers.update_end_condition(np.ones(10))
+        assert walkers.calculate_end_cond()
+        walkers.update_end_condition(np.zeros(10))
+        assert not walkers.calculate_end_cond()
+
+    def test_calculate_distance(self, walkers):
+        # TODO: check properly the calculations
+        walkers.calc_distances()
+
+    def test_alive_compas(self, walkers):
+        end_cond = torch.ones_like(walkers.end_condition)
+        end_cond[3] = 0
+        walkers.end_condition = end_cond
+        compas = walkers.get_alive_compas()
+        assert torch.all(compas == 3), "Type of end_cond: {} end_cond: {}: alive ix: {}".format(
+            type(end_cond), end_cond, walkers.alive_mask
+        )
+        assert len(compas.shape) == 1
+
+    def test_update_clone_probs(self, walkers):
+        walkers.reset()
+        walkers.virtual_rewards[:] = relativize(torch.arange(walkers.n).float().view(-1, 1))
+        walkers.update_clone_probs()
+        assert 0 < torch.sum(walkers.clone_probs == walkers.clone_probs[0]).cpu().item(),   \
+            (walkers.virtual_rewards, walkers.clone_probs)
+        walkers.reset()
+        walkers.update_clone_probs()
+        assert torch.sum(walkers.clone_probs == walkers.clone_probs[0]).item() == walkers.n
+        assert walkers.clone_probs.shape[0] == walkers.n
+        assert walkers.clone_probs.shape[1] == 1
+        assert len(walkers.clone_probs.shape) == 2
+
+    def test_balance(self, walkers):
+        walkers.reset()
+        walkers.balance()
+        assert walkers.will_clone.sum() == 0
+
+    def test_accumulate_rewards(self, walkers):
+        walkers.reset()
+
+
