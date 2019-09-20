b1e138c9af67520941b55ba629a44eabb633a7d0
Guillem Duran <guillem@sourced.tech>
2019-09-20 20:58:13 +0200

    Add atari environment

---
+++ fragile/atari/walkers.py
@@ -0,0 +1,24 @@
+from fragile.core.walkers import Walkers
+
+
+class AtariWalkers(Walkers):
+    """
+    This class is in charge of performing all the mathematical operations involved in evolving a \
+    cloud of walkers.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(AtariWalkers, self).__init__(*args, **kwargs)
+
+    def calculate_end_condition(self) -> bool:
+        """
+        Process data from the current state to decide if the iteration process should stop.
+
+        Returns:
+            Boolean indicating if the iteration process should be finished. True means \
+            it should be stopped, and False means it should continue.
+
+        """
+        end = super(AtariWalkers, self).calculate_end_condition()
+        return self.env_states.game_ends.all() and end
