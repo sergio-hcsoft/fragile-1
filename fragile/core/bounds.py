7b36612ac9c2cf981003548b3a2323fe5fb21b70
guillemdb <guillem.db@gmail.com>
2020-02-24 14:32:40 +0100

    Fix, document, and test Bounds

--- fragile/core/bounds.py
+++ fragile/core/bounds.py
@@ -1,24 +1,68 @@
-from typing import Union, Optional
+from collections import Iterable as _Iterable
+from typing import Iterable, Tuple, Union, Optional
 
 import numpy as np
 
+from fragile.core.utils import Scalar
+
 
 class Bounds:
+    """
+    The :class:`Bounds` implements the logic for defining and managing open intervals, \
+    and checking if a numpy array's values are inside a given interval.
+
+    It is used on a numpy array of a target shape.
+    """
+
     def __init__(
         self,
-        high: Union[np.ndarray, float, int] = np.inf,
-        low: Union[np.ndarray, float, int] = -np.inf,
+        high: Union[np.ndarray, Scalar] = np.inf,
+        low: Union[np.ndarray, Scalar] = -np.inf,
         shape: Optional[tuple] = None,
-        dtype: type = None,
+        dtype: Optional[type] = None,
     ):
+        """
+        Initialize a :class:`Bounds`.
 
+        Args:
+            high: Higher value for the bound interval. If it is an scalar it will be applied to \
+                  all the coordinates of a target vector. If it is a vector, the bounds will be \
+                  checked coordinate-wise. It defines and open interval.
+            low: Lower value for the bound interval. If it is an scalar it will be applied to \
+                  all the coordinates of a target vector. If it is a vector, the bounds will be \
+                  checked coordinate-wise. It defines and open interval.
+            shape: Shape of the array that will be bounded. Only needed if `high` and `low` are \
+                   and it is used to defined the dimensions that will be bounded.
+            dtype:  Data type of the array that will be bounded. It can be inferred from `high` \
+                    or `low` (the type of `high` takes priority).
+
+        Examples:
+            Initializing :class:`Bounds` using  numpy arrays.
+            >>> import numpy
+            >>> high, low = numpy.ones(3, dtype=float), -1 * numpy.ones(3, dtype=int)
+            >>> bounds = Bounds(high=high, low=low)
+            >>> print(bounds)
+            Bounds shape float64 dtype (3,) low [-1 -1 -1] high [1. 1. 1.]
+
+            Initializing :class:`Bounds` using  scalars.
+            >>> import numpy
+            >>> high, low, shape = 4, 2.1, (5,)
+            >>> bounds = Bounds(high=high, low=low, shape=shape)
+            >>> print(bounds)
+            Bounds shape float64 dtype (5,) low [2.1 2.1 2.1 2.1 2.1] high [4. 4. 4. 4. 4.]
+        """
+        # Infer shape if not specified
         if shape is None and hasattr(high, "shape"):
             shape = high.shape
         elif shape is None and hasattr(low, "shape"):
             shape = low.shape
-        self.shape = shape
-        if self.shape is None:
+        elif shape is None:
             raise TypeError("If shape is None high or low need to have .shape attribute.")
+        # High and low will be arrays of target shape
+        if not isinstance(high, np.ndarray):
+            high = np.array(high) if isinstance(high, _Iterable) else np.ones(shape) * high
+        if not isinstance(low, np.ndarray):
+            low = np.array(low) if isinstance(low, _Iterable) else np.ones(shape) * low
         self.high = high
         self.low = low
         if dtype is not None:
@@ -28,15 +72,42 @@ class Bounds:
         elif hasattr(low, "dtype"):
             self.dtype = low.dtype
         else:
-            self.dtype = type(low)
+            self.dtype = type(high) if high is not None else type(low)
 
     def __repr__(self):
         return "{} shape {} dtype {} low {} high {}".format(
             self.__class__.__name__, self.dtype, self.shape, self.low, self.high
         )
 
+    @property
+    def shape(self) -> Tuple:
+        """
+        Get the shape of the current bounds.
+
+        Returns:
+            tuple containing the shape of `high` and `low`
+        """
+        return self.high.shape
+
     @classmethod
-    def from_tuples(cls, bounds) -> "Bounds":
+    def from_tuples(cls, bounds: Iterable[tuple]) -> "Bounds":
+        """
+        Instantiate a :class:`Bounds` from a collection of tuples containig the higher and lower \
+        bounds for every dimension as a tuple.
+
+        Args:
+            bounds: Iterable that returns tuples containing the higher and lower bound for every \
+                    dimension of the target bounds.
+
+        Returns:
+                :class:`Bounds` instance.
+
+        Examples:
+            >>> intervals = ((-1, 1), (-2, 1), (2, 3))
+            >>> bounds = Bounds.from_tuples(intervals)
+            >>> print(bounds)
+            Bounds shape int64 dtype (3,) low [-1 -2  2] high [1 1 3]
+        """
         low, high = [], []
         for lo, hi in bounds:
             low.append(lo)
@@ -44,35 +115,146 @@ class Bounds:
         low, high = np.array(low), np.array(high)
         return Bounds(low=low, high=high)
 
-    @classmethod
-    def from_array(cls, x, scale: float = 1.0) -> "Bounds":
+    @staticmethod
+    def get_scaled_intervals(low: Union[np.ndarray, float, int],
+                             high: Union[np.ndarray, float, int],
+                             scale: float) -> Tuple[Union[np.ndarray, float],
+                                                    Union[np.ndarray, float]]:
         """
+        Scale the high and low vectors by an scale factor.
+
+        The value of the high and low will be proportional to the maximum and minimum values of \
+        the array. Scale defines the proportion to make the bounds bigger and smaller. For \
+        example, if scale is 1.1 the higher bound will be 10% higher, and the lower bounds 10% \
+        smaller. If scale is 0.9 the higher bound will be 10% lowe, and the lower bound 10% higher.
+        If scale is one, `high` and `low` will be equal to the maximum and minimum values of the  \
+        array.
 
         Args:
-            x:
-            scale:
+            high: Higher bound to be scaled.
+            low: Lower bound to be scaled.
+            scale: Value representing the tolerance in percentage from the current maximum and \
+            minimum values of the array.
 
         Returns:
+            :class:`Bounds` instance."""
+        pct = scale - 1
+        big_scale = 1 + np.abs(pct)
+        small_scale = 1 - np.abs(pct)
+        if pct > 0:
+            xmin_scaled = np.where(low < 0, low * big_scale, low * small_scale)
+            xmax_scaled = np.where(high < 0, high * small_scale, high * big_scale)
+        else:
+            xmin_scaled = np.where(low < 0, low * small_scale, low * small_scale)
+            xmax_scaled = np.where(high < 0, high * big_scale, high * small_scale)
+        return xmin_scaled, xmax_scaled
 
+    @classmethod
+    def from_array(cls, x: np.ndarray, scale: float = 1.0) -> "Bounds":
         """
-        scaled = x
-        xmin, xmax = scaled.min(axis=0), scaled.max(axis=0)
-        xmin_scaled = np.where(xmin < 0, xmin * scale, xmin / scale)
-        xmax_scaled = np.where(xmax < 0, xmax / scale, xmax * scale)
+        Instantiate a bounds compatible for bounding the given array. It also allows to set a \
+        margin for the high and low values.
+
+        The value of the high and low will be proportional to the maximum and minimum values of \
+        the array. Scale defines the proportion to make the bounds bigger and smaller. For \
+        example, if scale is 1.1 the higher bound will be 10% higher, and the lower bounds 10% \
+        smaller. If scale is 0.9 the higher bound will be 10% lowe, and the lower bound 10% higher.
+        If scale is one, `high` and `low` will be equal to the maximum and minimum values of the  \
+        array.
+
+        Args:
+            x: Numpy array used to initialize the bounds.
+            scale: Value representing the tolerance in percentage from the current maximum and \
+            minimum values of the array.
+
+        Returns:
+            :class:`Bounds` instance.
+
+        Examples:
+            >>> import numpy
+            >>> x = numpy.ones((3, 3))
+            >>> x[1:-1, 1:-1] = -5
+            >>> bounds = Bounds.from_array(x, scale=1.5)
+            >>> print(bounds)
+            Bounds shape float64 dtype (3,) low [ 0.5 -7.5  0.5] high [1.5 1.5 1.5]
+        """
+        xmin, xmax = x.min(axis=0), x.max(axis=0)
+        xmin_scaled, xmax_scaled = cls.get_scaled_intervals(xmin, xmax, scale)
         return Bounds(low=xmin_scaled, high=xmax_scaled)
 
-    def clip(self, points):
-        return np.clip(points, self.low, self.high)
+    def clip(self, x: np.ndarray) -> np.ndarray:
+        """
+        Clip the values of the target array to fall inside the bounds (closed interval).
 
-    def points_in_bounds(self, points: np.ndarray) -> np.ndarray:
-        return (self.clip(points) == points).all(axis=1).flatten()
+        Args:
+            x: Numpy array to be clipped.
 
-    def safe_margin(self, high, low) -> "Bounds":
-        xmin, xmax = self.low, self.high
-        xmin_scaled = np.where(xmin < 0, xmin * low, xmin / low)
-        xmax_scaled = np.where(xmax < 0, xmax / high, xmax * high)
+        Returns:
+            Clipped numpy array with all its values inside the defined bounds.
+        """
+        return np.clip(x, self.low, self.high)
+
+    def points_in_bounds(self, x: np.ndarray) -> Union[np.ndarray, bool]:
+        """
+        Checks if the rows of the target array have all their coordinates inside \
+        specified bounds.
+
+        If the array is one dimensional it will return a boolean, otherwise a vector of booleans.
+
+        Args:
+            x: Array to be checked against the bounds.
+
+        Returns:
+            Numpy array of booleans indicating if a row lies inside the bounds.
+        """
+        match = self.clip(x) == x
+        return match.all(axis=1).flatten() if len(match.shape) > 1 else match.all()
+
+    def safe_margin(
+        self,
+        low: Union[np.ndarray, float] = None,
+        high: Optional[Union[np.ndarray, float]] = None,
+        scale: float = 1.0,
+    ) -> "Bounds":
+        """
+        Initialize a new :class:`Bounds` with its bounds increased o decreased \
+        by an scale factor.
+
+        This is done multiplying both high and low for a given factor. The value of the new \
+        high and low will be proportional to the maximum and minimum values of \
+        the array. Scale defines the proportion to make the bounds bigger and smaller. For \
+        example, if scale is 1.1 the higher bound will be 10% higher, and the lower bounds 10% \
+        smaller. If scale is 0.9 the higher bound will be 10% lowe, and the lower bound 10% higher.
+        If scale is one, `high` and `low` will be equal to the maximum and minimum values of the  \
+        array.
+
+        Args:
+            high: Used to scale the `high` value of the current instance.
+            low: Used to scale the `low` value of the current instance.
+            scale: Value representing the tolerance in percentage from the current maximum and \
+            minimum values of the array.
+
+        Returns:
+            :class:`Bounds` with scaled high and low values.
+        """
+        xmax = self.high if high is None else high
+        xmin = self.low if low is None else low
+        xmin_scaled, xmax_scaled = self.get_scaled_intervals(xmin, xmax, scale)
         return Bounds(low=xmin_scaled, high=xmax_scaled)
 
-    def to_lims(self):
-        xlim, ylim = (self.low[0], self.high[0]), (self.low[1], self.high[1])
-        return xlim, ylim
+    def to_tuples(self) -> Tuple[Tuple[Scalar, Scalar], ...]:
+        """
+        Return a tuple of tuples containing the lower and higher bound for each \
+        coordinate of the :class:`Bounds` shape.
+
+        Returns:
+            Tuple of the form ((x0_low, x0_high), (x1_low, x1_high), ...,\
+              (xn_low, xn_high))
+        Examples:
+            >>> import numpy
+            >>> array = numpy.array([1, 2, 5])
+            >>> bounds = Bounds(high=array, low=-array)
+            >>> print(bounds.to_tuples())
+            ((-1, 1), (-2, 2), (-5, 5))
+        """
+        return tuple([dim for dim in zip(self.low, self.high)])
---
+++ fragile/tests/core/test_bounds.py
@@ -0,0 +1,177 @@
+import numpy
+import pytest
+
+from fragile.core.bounds import Bounds
+
+
+def create_bounds(name):
+    if name == "scalars":
+        return lambda: Bounds(high=5, low=-5, shape=(3,))
+    elif name == "high_array":
+        return lambda: Bounds(high=numpy.array([1, 2, 5]), low=-5)
+    elif name == "low_array":
+        return lambda: Bounds(low=numpy.array([-1, -5, -3]), high=5)
+    elif name == "both_array":
+        array = numpy.array([1, 2, 5])
+        return lambda: Bounds(high=array, low=-array)
+    elif name == "high_list":
+        return lambda: Bounds(low=numpy.array([-5, -2, -3]), high=[5, 5, 5])
+
+
+@pytest.fixture(scope="module")
+def bounds_fixture(request) -> Bounds:
+    return create_bounds(request.param)()
+
+
+class TestBounds:
+    bounds_fixture_params = ["scalars", "high_array", "low_array", "both_array", "high_list"]
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_init(self, bounds_fixture):
+        assert bounds_fixture.dtype is not None
+        assert isinstance(bounds_fixture.low, numpy.ndarray)
+        assert isinstance(bounds_fixture.high, numpy.ndarray)
+        assert isinstance(bounds_fixture.shape, tuple)
+        assert bounds_fixture.low.shape == bounds_fixture.shape
+        assert bounds_fixture.high.shape == bounds_fixture.shape
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_shape(self, bounds_fixture: Bounds):
+        shape = bounds_fixture.shape
+        assert isinstance(shape, tuple)
+        assert shape == (3,)
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_points_in_bounds(self, bounds_fixture):
+        points = numpy.array([[0, 0, 0], [11, 0, 0], [0, 11, 0], [11, 11, 11]])
+        res = bounds_fixture.points_in_bounds(points)
+        for a, b in zip(res.tolist(), [True, False, False, False]):
+            assert a == b
+
+    def test_from_tuples(self):
+        tup = ((-1, 2), (-3, 4), (2, 5))
+        bounds = Bounds.from_tuples(tup)
+        assert (bounds.low == numpy.array([-1, -3, 2])).all()
+        assert (bounds.high == numpy.array([2, 4, 5])).all()
+
+    def test_from_array(self):
+        array = numpy.array([[0, 0, 0], [11, 0, 0], [0, 11, 0], [11, 11, 11]])
+        bounds = Bounds.from_array(array)
+        assert (bounds.low == numpy.array([0, 0, 0])).all()
+        assert (bounds.high == numpy.array([11, 11, 11])).all()
+        assert bounds.shape == (3,)
+
+    def test_from_array_with_scale_positive(self):
+        array = numpy.array([[0, 0, 0], [10, 0, 0], [0, 10, 0], [10, 10, 10]])
+        bounds = Bounds.from_array(array, scale=1.1)
+        assert (bounds.low == numpy.array([0, 0, 0])).all(), (bounds.low, array.min(axis=0))
+        assert (bounds.high == numpy.array([11, 11, 11])).all(), (bounds.high, array.max(axis=0))
+        assert bounds.shape == (3,)
+
+        array = numpy.array([[-10, 0, 0], [-10, 0, 0], [0, -10, 0], [-10, -10, -10]])
+        bounds = Bounds.from_array(array, scale=1.1)
+        assert (bounds.high == numpy.array([0, 0, 0])).all(), (bounds.high, array.max(axis=0))
+        assert (bounds.low == numpy.array([-11, -11, -11])).all(), (bounds.low, array.min(axis=0))
+        assert bounds.shape == (3,)
+
+        array = numpy.array([[10, 10, 10], [100, 10, 10], [10, 100, 10], [100, 100, 100]])
+        bounds = Bounds.from_array(array, scale=1.1)
+        assert numpy.allclose(bounds.low, numpy.array([9.0, 9.0, 9])), (
+            bounds.low,
+            array.min(axis=0),
+        )
+        assert numpy.allclose(bounds.high, numpy.array([110, 110, 110])), (
+            bounds.high,
+            array.max(axis=0),
+        )
+        assert bounds.shape == (3,)
+
+    def test_from_array_with_scale_negative(self):
+        # high +, low +, scale > 1
+        array = numpy.array([[-10, 0, 0], [-10, 0, 0], [0, -10, 0], [-10, -10, -10]])
+        bounds = Bounds.from_array(array, scale=0.9)
+        assert (bounds.high == numpy.array([0, 0, 0])).all(), (bounds.high, array.max(axis=0))
+        assert (bounds.low == numpy.array([-9, -9, -9])).all(), (bounds.low, array.min(axis=0))
+        assert bounds.shape == (3,)
+        array = numpy.array([[0, 0, 0], [10, 0, 0], [0, 10, 0], [10, 10, 10]])
+        bounds = Bounds.from_array(array, scale=0.9)
+        assert (bounds.low == numpy.array([0, 0, 0])).all(), (bounds, array)
+        assert (bounds.high == numpy.array([9, 9, 9])).all()
+        assert bounds.shape == (3,)
+        # high +, low +, scale < 1
+        array = numpy.array([[10, 10, 10], [100, 10, 10], [10, 100, 10], [100, 100, 100]])
+        bounds = Bounds.from_array(array, scale=0.9)
+        assert numpy.allclose(bounds.low, numpy.array([9, 9, 9])), (bounds.low, array.min(axis=0))
+        assert numpy.allclose(bounds.high, numpy.array([90, 90, 90])), (
+            bounds.high,
+            array.max(axis=0),
+        )
+        assert bounds.shape == (3,)
+        # high -, low -, scale > 1
+        array = numpy.array(
+            [[-100, -10, -10], [-100, -10, -10], [-10, -100, -10], [-100, -100, -100]]
+        )
+        bounds = Bounds.from_array(array, scale=1.1)
+        assert numpy.allclose(bounds.high, numpy.array([-9, -9, -9])), (
+            bounds.high,
+            array.max(axis=0),
+        )
+        assert numpy.allclose(bounds.low, numpy.array([-110, -110, -110])), (
+            bounds.low,
+            array.min(axis=0),
+        )
+        assert bounds.shape == (3,)
+        # high -, low -, scale < 1
+        array = numpy.array(
+            [[-100, -10, -10], [-100, -10, -10], [-10, -100, -10], [-100, -100, -100]]
+        )
+        bounds = Bounds.from_array(array, scale=0.9)
+        assert numpy.allclose(bounds.high, numpy.array([-11, -11, -11])), (
+            bounds.high,
+            array.max(axis=0),
+        )
+        assert numpy.allclose(bounds.low, numpy.array([-90, -90, -90])), (
+            bounds.low,
+            array.min(axis=0),
+        )
+        assert bounds.shape == (3,)
+
+    def test_clip(self):
+        tup = ((-1, 10), (-3, 4), (2, 5))
+        array = numpy.array([[-10, 0, 0], [11, 0, 0], [0, 11, 0], [11, 11, 11]])
+        bounds = Bounds.from_tuples(tup)
+        clipped = bounds.clip(array)
+        target = numpy.array([[-1, 0, 2], [10, 0, 2], [0, 4, 2], [10, 4, 5]])
+        assert numpy.allclose(clipped, target), (clipped, target)
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_to_tuples(self, bounds_fixture):
+        tuples = bounds_fixture.to_tuples()
+        assert len(tuples) == 3
+        assert min([x[0] for x in tuples]) == -5
+        assert max([x[1] for x in tuples]) == 5
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_points_in_bounds(self, bounds_fixture):
+        zeros = numpy.zeros((3, 3))
+        assert all(bounds_fixture.points_in_bounds(zeros))
+        tens = numpy.full_like(zeros, 10)
+        assert not any(bounds_fixture.points_in_bounds(tens))
+        tens = numpy.array([[-10, 0, 1], [0, 0, 0], [10, 10, 10]])
+        assert sum(bounds_fixture.points_in_bounds(tens)) == 1
+
+    @pytest.mark.parametrize("bounds_fixture", bounds_fixture_params, indirect=True)
+    def test_safe_margin(self, bounds_fixture: Bounds):
+        new_bounds = bounds_fixture.safe_margin()
+        assert numpy.allclose(new_bounds.low, bounds_fixture.low)
+        assert numpy.allclose(new_bounds.high, bounds_fixture.high)
+        low = numpy.full_like(bounds_fixture.low, -10)
+        new_bounds = bounds_fixture.safe_margin(low=low)
+        assert numpy.allclose(new_bounds.high, bounds_fixture.high)
+        assert numpy.allclose(new_bounds.low, low)
+        new_bounds = bounds_fixture.safe_margin(low=low, scale=2)
+        assert numpy.allclose(new_bounds.high, bounds_fixture.high * 2)
+        assert numpy.allclose(new_bounds.low, low * 2)
+
+
+
