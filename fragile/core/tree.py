7fc177656f872e8b461c321c0e37e4155fedde12
guillemdb <guillem.db@gmail.com>
2020-03-07 19:39:57 +0100

    Fix root node removed. Document file

--- fragile/core/tree.py
+++ fragile/core/tree.py
@@ -1,17 +1,17 @@
 import copy
-from typing import List
+from typing import Any, List, Set
 
 import networkx as nx
 
-from fragile.core.base_classes import BaseStateTree, States
-from fragile.core.walkers import StatesWalkers
+from fragile.core.base_classes import BaseStateTree
+from fragile.core.states import StatesEnv, StatesModel, StatesWalkers
 
 
 class _BaseNetworkxTree(BaseStateTree):
-    """This is a tree data structure that stores the paths followed by the walkers. It can be
-    pruned to delete paths that are longer be needed. It uses a networkx Graph. If someone
-    wants to spend time building a proper data structure, please make a PR, and I will be super
-    happy!
+    """
+    This is a tree data structure that stores the paths followed by the walkers. \
+    It can be pruned to delete paths that are longer be needed. It uses a \
+    networkx DiGraph to keep track of the states relationships.
     """
 
     ROOT_ID = 0
@@ -22,24 +22,22 @@ class _BaseNetworkxTree(BaseStateTree):
         self.data: nx.DiGraph = nx.DiGraph()
         self.data.add_node(self.ROOT_ID, state=None, n_iter=-1)
         self.root_id = self.ROOT_ID
-        self.node_names = {self.ROOT_ID: self.ROOT_HASH}
-        self.names_to_hash = {self.ROOT_HASH: self.ROOT_ID}
+        self.ids_to_hash = {self.ROOT_ID: self.ROOT_HASH}
+        self.hash_to_ids = {self.ROOT_HASH: self.ROOT_ID}
         self._node_count = 0
         self.leafs = {self.ROOT_ID}
 
     def reset(
         self,
-        parent_ids: List[int] = None,
-        env_states: States = None,
-        model_states: States = None,
-        walkers_states: States = None,
+        env_states: StatesEnv = None,
+        model_states: StatesModel = None,
+        walkers_states: StatesWalkers = None,
     ) -> None:
         """
         Delete all the data currently stored and reset the internal state of \
         the tree .
 
         Args:
-            parent_ids: Ignored. Only to implement interface.
             env_states: Ignored. Only to implement interface.
             model_states: Ignored. Only to implement interface.
             walkers_states: Ignored. Only to implement interface.
@@ -50,55 +48,73 @@ class _BaseNetworkxTree(BaseStateTree):
         self.data: nx.DiGraph = nx.DiGraph()
         self.data.add_node(self.ROOT_ID, state=None, n_iter=-1)
         self.root_id = self.ROOT_ID
-        self.node_names = {self.ROOT_ID: self.ROOT_HASH}
-        self.names_to_hash = {self.ROOT_HASH: self.ROOT_ID}
+        self.ids_to_hash = {self.ROOT_ID: self.ROOT_HASH}
+        self.hash_to_ids = {self.ROOT_HASH: self.ROOT_ID}
         self._node_count = 0
         self.leafs = {self.ROOT_ID}
 
-    def get_update_hash(self, node_hash: int) -> None:
+    def add_new_hash(self, node_hash: int) -> int:
         """
-
-        Args:
-            node_hash: Unique identifier of a Node.
-
-        Returns:
-            None.
+        Generate and assign a node id to a new node hash and return the node_id \
+        assigned to it.
         """
-        node_name = self.node_names.get(node_hash, None)
-        if node_name is None:
-            node_name = self.update_hash(node_hash)
-        return node_name
-
-    def update_hash(self, node_hash):
+        if node_hash in self.hash_to_ids:
+            raise ValueError("Hash is already present with id %s " % self.ids_to_hash[node_hash])
         self._node_count += 1
-        node_name = int(self._node_count)
-        self.node_names[node_hash] = node_name
-        self.names_to_hash[node_name] = node_hash
-        return node_name
+        node_id = int(self._node_count)
+        self.ids_to_hash[node_id] = node_hash
+        self.hash_to_ids[node_hash] = node_id
+        return node_id
 
     def append_leaf(
         self,
         leaf_id: int,
         parent_id: int,
-        state,
-        action,
-        dt: int,
+        state: Any,
+        action: Any = 1,
+        dt: int = None,
         n_iter: int = None,
         from_hash: bool = False,
         reward: float = 0.0,
         cum_reward: float = 0.0,
     ):
         """
-        Add a new state as a leaf node of the tree to keep track of the trajectories of the swarm.
-        :param leaf_id: Id that identifies the state that will be added to the tree.
-        :param parent_id: id that references the state of the system before taking the action.
-        :param state: observation assigned to leaf_id state.
-        :param action: action taken at leaf_id state.
-        :param dt: parameters taken into account when integrating the action.
-        :return:
+        Add a new state as a leaf node of the tree to keep track of the \
+        trajectories of the swarm.
+
+        Args:
+            leaf_id: Id that identifies the state that will be added to the tree. \
+                     If the id is the hash of a walker state ``from_hash`` needs \
+                     to be ``True``. Otherwise it refers to a node_id the node \
+                     will be assigned.
+            parent_id: Id that identifies the state of the node parent that will \
+                       be added to the tree. If the id is the hash of a walker state \
+                       ``from_hash`` needs to be ``True``. Otherwise it refers to \
+                       the parent node_id.
+            state: observation assigned to ``leaf_id`` node.
+            action: action taken at ``leaf_id`` node.
+            dt: Number of steps taken from the parent node to ``leaf_id``.
+            n_iter: Swarm iteration when the current node was generated.
+            from_hash: If  ``True`` ``node_id`` and ``parent_id`` will be \
+                      considered hashes of states. If ``False`` they will be \
+                      considered node ids.
+            reward: Instantaneous reward assigned to the node that will be added.
+            cum_reward: Cumulative reward assigned to the node that will be added.
+
+        Returns:
+
         """
-        leaf_name = self.update_hash(leaf_id) if from_hash else leaf_id
-        parent_name = self.node_names[parent_id] if from_hash else parent_id
+        leaf_name = (
+            (
+                self.add_new_hash(leaf_id)
+                if leaf_id not in self.hash_to_ids
+                else self.hash_to_ids[leaf_id]
+            )
+            if from_hash
+            else leaf_id
+        )
+
+        parent_name = self.hash_to_ids[parent_id] if from_hash else parent_id
         if leaf_name not in self.data.nodes and leaf_name != parent_name:
             self.data.add_node(
                 leaf_name, state=state, n_iter=n_iter, reward=reward, cum_reward=cum_reward
@@ -106,35 +122,79 @@ class _BaseNetworkxTree(BaseStateTree):
             self.data.add_edge(parent_name, leaf_name, action=action, dt=dt)
             self.leafs.add(leaf_name)
 
-    def prune_tree(self, dead_leafs, alive_leafs, from_hash: bool = False):
-        """This prunes the orphan leaves that will no longer be used to save memory."""
+    def prune_tree(self, dead_leafs: Set[int], alive_leafs: Set[int], from_hash: bool = False):
+        """
+        Prune the orphan leaves that will no longer be used in order to save memory.
+
+        Args:
+            dead_leafs: Leaves of the branches that will be removed.
+            alive_leafs: Leaves of the branches that will kept being expanded.
+            from_hash: If ``True`` ``dead_leafs`` and ``alive_leafs`` will be \
+                      considered hashes of states. If ``False`` they will be \
+                      considered node ids.
+
+        Returns:
+            None
+        """
         for leaf in dead_leafs:
             self.prune_branch(leaf, alive_leafs, from_hash=from_hash)
         return
 
     def get_branch(self, leaf_id, from_hash: bool = False, root=ROOT_ID) -> tuple:
         """
-        Get the observation from the game ended at leaf_id
-        :param leaf_id: id of the leaf node belonging to the branch that will be recovered.
-        :return: Sequence of observations belonging to a given branch of the tree.
+        Get the data of the branch ended at leaf_id.
+
+        Args:
+            leaf_id: Id that identifies the leaf of the tree. \
+                     If ``leaf_id`` is the hash of a walker state ``from_hash`` \
+                     needs to be ``True``. Otherwise it refers to a node id of \
+                     the leaf node.
+            from_hash: If  ``True`` ``leaf_id`` is considered the hash of walker \
+                      state. If ``False`` it will be considered a node id.
+            root: Node id of the root node of the tree.
+
+        Returns:
+            tuple containing (states, actions, dts) that represent the history \
+            of a given branch of the tree.
+
+            ``states`` represent the :class:`StatesEnv`.states assigned to each node.
+            ``actions`` represent the :class:`StatesModel`.actions taken at each state.
+            ``dts`` the :class:`StatesModel`.dt of each state.
+
         """
-        leaf_name = self.node_names[leaf_id] if from_hash else leaf_id
+        leaf_name = self.hash_to_ids[leaf_id] if from_hash else leaf_id
         nodes = nx.shortest_path(self.data, root, leaf_name)
         states = [self.data.nodes[n]["state"] for n in nodes]
         actions = [self.data.edges[(n, nodes[i + 1])]["action"] for i, n in enumerate(nodes[:-1])]
         dts = [self.data.edges[(n, nodes[i + 1])]["dt"] for i, n in enumerate(nodes[:-1])]
         return states, actions, dts
 
-    def prune_branch(self, leaf_id, alive_leafs, from_hash: bool = False):
-        """This recursively prunes a branch that only leads to an orphan leaf."""
-        leaf = self.node_names[leaf_id] if from_hash else leaf_id
+    def prune_branch(self, leaf_id: int, alive_leafs: set, from_hash: bool = False):
+        """
+        Recursively prunes a branch that ends in an orphan leaf.
+
+        Args:
+            leaf_id: Id that identifies the leaf of the tree. \
+                     If ``leaf_id`` is the hash of a walker state ``from_hash`` \
+                     needs to be ``True``. Otherwise it refers to a node id of \
+                     the leaf node.
+            alive_leafs: Leaves of the branches that will kept being expanded.
+            from_hash: If ``True`` ``leaf_id`` and ``alive_leafs`` will be \
+                      considered hashes of states. If ``False`` they will be \
+                      considered node ids.
+
+        Returns:
+            None
+
+        """
+        leaf = self.hash_to_ids[leaf_id] if from_hash else leaf_id
         is_not_a_leaf = len(self.data.out_edges([leaf])) > 0
-        if is_not_a_leaf:
+        if is_not_a_leaf and leaf != self.ROOT_ID:
             self.leafs.discard(leaf)
             return
         elif leaf == self.ROOT_ID or leaf not in self.data.nodes:
             return
-        alive_leafs = set([self.node_names[le] if from_hash else le for le in set(alive_leafs)])
+        alive_leafs = set([self.hash_to_ids[le] if from_hash else le for le in set(alive_leafs)])
         if leaf in alive_leafs:
             return
         parents = set(self.data.in_edges([leaf]))
@@ -143,10 +203,12 @@ class _BaseNetworkxTree(BaseStateTree):
         for parent, _ in parents:
             return self.prune_branch(parent, alive_leafs)
 
-    def get_parent(self, node_id):
+    def get_parent(self, node_id) -> int:
+        """Get the node id of the parent of the target node."""
         return list(self.data.in_edges(node_id))[0][0]
 
-    def get_leaf_nodes(self):
+    def get_leaf_nodes(self) -> List[int]:
+        """Return a list containing all the node ids of the leaves of the tree."""
         leafs = []
         for node in self.data.nodes:
             if len(self.data.out_edges([node])) == 0:
@@ -158,11 +220,30 @@ class HistoryTree(_BaseNetworkxTree):
     def add_states(
         self,
         parent_ids: List[int],
-        env_states: States = None,
-        model_states: States = None,
+        env_states: StatesEnv = None,
+        model_states: StatesModel = None,
         walkers_states: StatesWalkers = None,
         n_iter: int = None,
     ):
+        """
+        Update the history of the tree adding the necessary data to recreate a \
+        the trajectories sampled by the :class:`Swarm`.
+
+        Args:
+            parent_ids: List of states hashes representing the parent nodes of \
+                        the current states.
+            env_states: :class:`StatesEnv` containing the data that will be \
+                        saved as new leaves in the tree.
+            model_states: :class:`StatesModel` containing the data that will be \
+                        saved as new leaves in the tree.
+            walkers_states: :class:`StatesWalkers` containing the data that will be \
+                        saved as new leaves in the tree.
+            n_iter: Number of iteration of the algorithm when the data was sampled.
+
+        Returns:
+            None
+
+        """
         leaf_ids = walkers_states.id_walkers.tolist()
         for i, (leaf, parent) in enumerate(zip(leaf_ids, parent_ids)):
             state = copy.deepcopy(env_states.states[i])
@@ -182,8 +263,21 @@ class HistoryTree(_BaseNetworkxTree):
                 cum_reward=cum_reward,
             )
 
-    def prune_tree(self, alive_leafs: set, from_hash: bool = False):
-        alive_leafs = set([self.node_names[le] if from_hash else le for le in set(alive_leafs)])
+    def prune_tree(self, alive_leafs: Set[int], from_hash: bool = False):
+        """
+        Remove the branches that do not have a walker in their leaves.
+
+        Args:
+            alive_leafs: Contains the ids  of the leaf nodes that are being \
+                         expanded by the walkers.
+            from_hash: from_hash: If ``True`` ``alive_leafs`` will be \
+                      considered a set of hashes of states. If ``False`` it \
+                      will be considered a set of node ids.
+
+        Returns:
+            None.
+        """
+        alive_leafs = set([self.hash_to_ids[le] if from_hash else le for le in set(alive_leafs)])
         dead_leafs = self.leafs - alive_leafs
         super(HistoryTree, self).prune_tree(
             dead_leafs=dead_leafs, alive_leafs=alive_leafs, from_hash=False
