ff36723baa900bc52c53a411c0e9b3d701836aaf
guillemdb <guillem.db@gmail.com>
2020-03-14 18:28:23 +0100

    Reduce ray memory usage in tests

--- tests/distributed/ray/__init__.py
+++ tests/distributed/ray/__init__.py
@@ -3,10 +3,12 @@ try:
 
     def init_ray():
         MB = 1024 * 1024
-        ray.init(redis_max_memory=500 * MB, object_store_memory=500 * MB, ignore_reinit_error=True)
+        ray.init(redis_max_memory=200 * MB, object_store_memory=200 * MB, ignore_reinit_error=True)
 
 
 except ImportError:
+
+    from fragile.distributed.ray import ray
 
     def init_ray():
         pass
