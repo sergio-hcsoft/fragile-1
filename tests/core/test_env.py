9cc0078071fb0b6a4d18b4689149ea72765dac08
guillemdb <guillem.db@gmail.com>
2020-03-14 12:14:59 +0100

    Improve test for Environment interface

--- tests/core/test_env.py
+++ tests/core/test_env.py
@@ -6,60 +6,74 @@ import pytest
 
 from fragile.core.bounds import Bounds
 from fragile.core.env import DiscreteEnv, Environment
-from fragile.core.states import States
+from fragile.core.states import StatesEnv, StatesModel
 from fragile.optimize.env import Function
 
 N_WALKERS = 10
 
 
+def atari_env():
+    env = AtariEnvironment(name="MsPacman-v0", clone_seeds=True, autoreset=True)
+    env.reset()
+    env = DiscreteEnv(env)
+    return env
+
+
+def classic_control_env():
+    env = ClassicControl()
+    env.reset()
+    env = DiscreteEnv(env)
+    return env
+
+
+def function_env():
+    bounds = Bounds(shape=(2,), high=1, low=1, dtype=int)
+    env = Function(function=lambda x: np.ones(N_WALKERS), bounds=bounds)
+    return env
+
+
 def create_env_and_model_states(name="classic") -> Callable:
-    def atari_env():
-        env = AtariEnvironment(name="MsPacman-v0", clone_seeds=True, autoreset=True)
-        env.reset()
-        env = DiscreteEnv(env)
+    def _atari_env():
+        env = atari_env()
         params = {"actions": {"dtype": np.int64}, "critic": {"dtype": np.float32}}
-        states = States(state_dict=params, batch_size=N_WALKERS)
+        states = StatesModel(state_dict=params, batch_size=N_WALKERS)
         states.update(actions=np.ones(N_WALKERS), critic=np.ones(N_WALKERS))
         return env, states
 
-    def classic_control_env():
-        env = ClassicControl()
-        env.reset()
-        env = DiscreteEnv(env)
+    def _classic_control_env():
+        env = classic_control_env()
         params = {"actions": {"dtype": np.int64}, "dt": {"dtype": np.float32}}
-        states = States(state_dict=params, batch_size=N_WALKERS)
+        states = StatesModel(state_dict=params, batch_size=N_WALKERS)
         states.update(actions=np.ones(N_WALKERS), dt=np.ones(N_WALKERS))
         return env, states
 
-    def function_env():
-        bounds = Bounds(shape=(2,), high=1, low=1, dtype=int)
-        env = Function(function=lambda x: np.ones(N_WALKERS), bounds=bounds)
+    def _function_env():
+        env = function_env()
         params = {"actions": {"dtype": np.int64, "size": (2,)}, "dt": {"dtype": np.float32}}
-        states = States(state_dict=params, batch_size=N_WALKERS)
+        states = StatesModel(state_dict=params, batch_size=N_WALKERS)
         return env, states
 
     if name.lower() == "pacman":
-        return atari_env
+        return _atari_env
     elif name.lower() == "function":
-        return function_env
+        return _function_env
     else:
-        return classic_control_env
+        return _classic_control_env
 
 
-@pytest.fixture(scope="module")
-def env_data(request) -> Tuple[Environment, States]:
-    if request.param in TestBaseEnvironment.env_fixtures_params:
-        env, model_states = create_env_and_model_states(request.param)()
-
-    else:
-        raise ValueError("Environment not well defined")
-    return env, model_states
+env_fixture_params = ["classic", "pacman", "function"]
 
 
-class TestBaseEnvironment:
-    env_fixtures_params = ["classic", "pacman", "function"]
+class TestEnvironment:
+    @pytest.fixture(params=env_fixture_params)
+    def env_data(self, request) -> Tuple[Environment, StatesModel]:
+        if request.param in env_fixture_params:
+            env, model_states = create_env_and_model_states(request.param)()
 
-    @pytest.mark.parametrize("env_data", env_fixtures_params, indirect=True)
+        else:
+            raise ValueError("Environment not well defined: %s" % request.param)
+        return env, model_states
+
     def test_reset(self, env_data):
         env, model_states = env_data
         states = env.reset()
@@ -71,7 +85,6 @@ class TestBaseEnvironment:
         for name in states.keys():
             assert states[name].shape[0] == batch_size
 
-    @pytest.mark.parametrize("env_data", env_fixtures_params, indirect=True)
     def test_get_params_dir(self, env_data):
         env, model_states = env_data
         params_dict = env.get_params_dict()
@@ -82,10 +95,34 @@ class TestBaseEnvironment:
             for ki in v.keys():
                 assert isinstance(ki, str)
 
-    @pytest.mark.parametrize("env_data", env_fixtures_params, indirect=True)
     def test_step(self, env_data):
         batch_size = 10
         env, model_states = env_data
         states = env.reset(batch_size=batch_size)
         new_states = env.step(model_states=model_states, env_states=states)
         assert new_states.ends.sum() == 0
+
+    def test_state_shape(self, env_data):
+        env, model_states = env_data
+        assert hasattr(env, "states_shape")
+        assert isinstance(env.states_shape, tuple)
+
+    def test_observs_shape(self, env_data):
+        env, model_states = env_data
+        assert hasattr(env, "observs_shape")
+        assert isinstance(env.observs_shape, tuple)
+
+    def test_states_from_data(self, env_data):
+        env, model_states = env_data
+        batch_size = 10
+        states = np.zeros((batch_size, 5)).tolist()
+        observs = np.ones((batch_size, 5)).tolist()
+        rewards = np.arange(batch_size).tolist()
+        ends = np.zeros(batch_size, dtype=bool).tolist()
+        state = env.states_from_data(
+            batch_size=batch_size, states=states, observs=observs, rewards=rewards, ends=ends
+        )
+        assert isinstance(state, StatesEnv)
+        for val in state.vals():
+            assert isinstance(val, np.ndarray)
+            assert len(val) == batch_size
