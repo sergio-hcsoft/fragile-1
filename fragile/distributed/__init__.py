9334a5a03e6072d4f056393ac3d19f1e58e365d9
guillemdb <guillem.db@gmail.com>
2020-03-14 16:19:49 +0100

    Update distributed imports

--- fragile/distributed/__init__.py
+++ fragile/distributed/__init__.py
@@ -2,8 +2,8 @@
 import sys
 
 try:
-    from fragile.distributed.ray.export_swarm import DistributedExport
-    from fragile.distributed.ray.env import RayEnv
+    from fragile.distributed.distributed_export import DistributedExport
+    from fragile.distributed.env import ParallelEnvironment, ParallelFunction, RayEnv
 except (ImportError, ModuleNotFoundError) as e:
     if sys.version_info == (3, 7):
         raise e
