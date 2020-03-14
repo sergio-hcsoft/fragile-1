0c83b8087d113466e92f12ae112394abdd298cc7
guillemdb <guillem.db@gmail.com>
2020-03-14 14:06:27 +0100

    Refactor and add tests for distributed swarm

--- fragile/distributed/__init__.py
+++ fragile/distributed/__init__.py
@@ -3,6 +3,7 @@ import sys
 
 try:
     from fragile.distributed.ray.export_swarm import DistributedExport
+    from fragile.distributed.ray.env import RayEnv
 except (ImportError, ModuleNotFoundError) as e:
     if sys.version_info == (3, 7):
         raise e
--- tests/distributed/ray/test_export_swarm.py
+++ tests/distributed/ray/test_export_swarm.py
@@ -2,27 +2,82 @@ import sys
 
 import pytest
 
-from fragile.distributed.ray.export_swarm import DistributedExport
-from tests.core.test_swarm import create_cartpole_swarm
+from fragile.distributed.distributed_export import DistributedExport
+from tests.core.test_swarm import create_cartpole_swarm, TestSwarm
 
+using_py38 = sys.version_info >= (3, 8)
+using_py36 = sys.version_info < (3, 7)
+only_py37 = pytest.mark.skipif(using_py38 or using_py36, reason="requires python3.7")
 
-@pytest.fixture()
-def ray_swarm(request=None):
-    if sys.version_info != (3, 7):
-        return DistributedExport(create_cartpole_swarm, n_swarms=2)
+swarm_types = [create_cartpole_swarm]
 
 
 class TestDistributedExport:
-    @pytest.mark.skipif(sys.version_info > (3, 7), reason="requires python3.7 or lower")
+    @pytest.fixture(params=swarm_types)
+    def ray_swarm(self, request):
+        swarm = DistributedExport(request.param, n_swarms=2)
+
+        def kill_swarm():
+            for e in swarm.swarms:
+                e.__ray_terminate__.remote()
+            swarm.param_server.__ray_terminate__.remote()
+
+        # request.addfinalizer(kill_swarm)
+        return swarm
+
+    @pytest.mark.skipif(using_py38, reason="requires python3.7 or lower")
     def test_init(self, ray_swarm):
         pass
 
-    @pytest.mark.skipif(sys.version_info > (3, 7), reason="requires python3.7 or lower")
+    @pytest.mark.skipif(using_py38, reason="requires python3.7 or lower")
     def test_reset(self, ray_swarm):
         ray_swarm.reset()
 
-    @pytest.mark.skipif(sys.version_info != (3, 7), reason="requires python3.7 or lower")
-    def test_run(self, ray_swarm):
-        ray_swarm.run()
-        best_walker = ray_swarm.get_best()
-        assert best_walker.rewards > 200
+
+def create_distributed_export():
+    return DistributedExport(create_cartpole_swarm, n_swarms=2)
+
+
+swarm_dict = {"export": create_distributed_export}
+swarm_names = list(swarm_dict.keys())
+test_scores = {
+    "export": 130,
+}
+
+
+@only_py37
+class TestExportInterface(TestSwarm):
+    @pytest.fixture(params=swarm_names)
+    def swarm(self, request):
+        swarm = swarm_dict.get(request.param, create_cartpole_swarm)()
+
+        def kill_swarm():
+            for e in swarm.swarms:
+                e.__ray_terminate__.remote()
+            swarm.param_server.__ray_terminate__.remote()
+
+        # request.addfinalizer(kill_swarm)
+        return swarm
+
+    @pytest.fixture(params=swarm_names)
+    def swarm_with_score(self, request):
+        swarm = swarm_dict.get(request.param, create_cartpole_swarm)()
+        score = test_scores[request.param]
+
+        def kill_swarm():
+            for e in swarm.swarms:
+                e.__ray_terminate__.remote()
+            swarm.param_server.__ray_terminate__.remote()
+
+        request.addfinalizer(kill_swarm)
+        return swarm, score
+
+    # Distributed Swarm does not implement the full interface
+    def test_env_init(self, swarm):
+        pass
+
+    def test_step_does_not_crashes(self, swarm):
+        pass
+
+    def test_attributes(self, swarm):
+        pass
